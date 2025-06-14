33ed5038a970c0bd2a32d95f305058a599521141
kavithachinnusamy20 <kavithachinnusamy20@gmail.com>
2025-06-14 14:42:36 -0400

    first commit

---
+++ Per_scholas_assignment/SBA_318_Express_Server_App/data/user.js
@@ -0,0 +1,20 @@
+[
+  {
+    "id": 1,
+    "name": "Kavitha",
+    "email": "kavitha@example.com",
+    "age": 28
+  },
+  {
+    "id": 2,
+    "name": "Anitha",
+    "email": "anitha@example.com",
+    "age": 34
+  },
+  {
+    "id": 3,
+    "name": "Nikitha",
+    "email": "nikitha@example.com",
+    "age": 25
+  }
+]
--- Per_scholas_assignment/SBA_318_Express_Server_App/index.js
+++ Per_scholas_assignment/SBA_318_Express_Server_App/index.js
@@ -3,11 +3,7 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-const magicResponse =["It is certain", "It is decidedly so", "Without a doubt", 
-    "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good",
-    "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", 
-    "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no",
-    "Outlook not so good", "Very doubtful"]
+
 
 
 
@@ -18,16 +14,26 @@ app.listen(port, () => {
     console.log('server listening at port ${port}')
 })
 
-app.get("/", (req, res) => {
-    let i = Math.floor(Math.random() * magicResponse.length)
-    res.render('home', { pageTitle : magicResponse[i]})
-}
-)
+// app.get("/", (req, res) => {
+//     let i = Math.floor(Math.random() * magicResponse.length)
+//     res.render('home', { pageTitle : magicResponse[i]})
+// }
+// )
 
 app.get("/news", (req, res) => {
     res.render('news')
 }
 )
+app.get('/users',(req,res)=> {
+  res.send('users')
+
+})
+
+
+app.post("/create",(req,res)=>{
+    res.send("hit")
+})
+
 
 app.use((req,res) => {
     res.status(404).render('404')
